b16e8930210381a63e0a774f3125c5feed6c5612
ebiyo <ariefridzki@gmail.com>
2025-05-27 14:13:44 +0700

    [REFACTOR] Added 'pendaftar ditolak' on lowongan detail

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    API_URL: 'https://hiringgo.syauqiyasman.com',
+    API_URL: 'http://localhost:8080',
     AUTH_ENDPOINT: '/auth',
     APP_NAME: 'HiringGo',
     APP_VERSION: '1.0.0',
